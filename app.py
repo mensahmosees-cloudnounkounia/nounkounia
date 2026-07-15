74ccbff79daaab72322f0df8c69450a5fe5c732e
Fondateur Nounkoun <mensahmoise12@gmail.com>
2026-07-15 10:01:08 -0500

    add home route

--- app.py
+++ app.py
@@ -59,3 +59,7 @@ def whatsapp():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
+
+@app.route("/", methods=["GET"])
+def home():
+    return "Nounkounia V8 LIVE - OK", 200
